c80245be3544884ba572c9c0ffd1d3087568c7c5
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2024-02-19 09:45:00 +0530

    feat: text docs completed

--- src/documentation/HeadingsDocs/HeadingDocs.jsx
+++ src/documentation/HeadingsDocs/HeadingDocs.jsx
@@ -19,28 +19,28 @@ export const HeadingDocs = () => {
             The text's style can be modified by passing the heading type and
             heading text as props to the component.
           </p>
-          <div className="heading-showcase">
-            <Heading headingType="h1" headingText="Heading level 1" />
-            <Heading headingType="h2" headingText="Heading level 2" />
-            <Heading headingType="h3" headingText="Heading level 3" />
-            <Heading headingType="h4" headingText="Heading level 4" />
-            <Heading headingType="h5" headingText="Heading level 5" />
-            <Heading headingType="h6" headingText="Heading level 6" />
-          </div>
-          <code>
-            <iframe
-              src="https://carbon.now.sh/embed?bg=rgba%28229%2C223%2C214%2C1%29&t=seti&wt=sharp&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253CHeading%2520headingType%253D%2522h1%2522%2520headingText%253D%2522Heading%2520level%25201%2522%252F%253E%250A%253CHeading%2520headingType%253D%2522h2%2522%2520headingText%253D%2522Heading%2520level%25202%2522%252F%253E%250A%253CHeading%2520headingType%253D%2522h3%2522%2520headingText%253D%2522Heading%2520level%25203%2522%252F%253E%250A%253CHeading%2520headingType%253D%2522h4%2522%2520headingText%253D%2522Heading%2520level%25204%2522%252F%253E%250A%253CHeading%2520headingType%253D%2522h5%2522%2520headingText%253D%2522Heading%2520level%25205%2522%252F%253E%250A%253CHeading%2520headingType%253D%2522h6%2522%2520headingText%253D%2522Heading%2520level%25206%2522%252F%253E%250A"
-              style={{
-              width: "768px",
-              height: "200px",
+        </div>
+        <div className="heading-showcase">
+          <Heading headingType="h1" headingText="Heading level 1" />
+          <Heading headingType="h2" headingText="Heading level 2" />
+          <Heading headingType="h3" headingText="Heading level 3" />
+          <Heading headingType="h4" headingText="Heading level 4" />
+          <Heading headingType="h5" headingText="Heading level 5" />
+          <Heading headingType="h6" headingText="Heading level 6" />
+        </div>
+        <code>
+          <iframe
+            src="https://carbon.now.sh/embed?bg=rgba%28229%2C223%2C214%2C1%29&t=seti&wt=sharp&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253CHeading%2520headingType%253D%2522h1%2522%2520headingText%253D%2522Heading%2520level%25201%2522%252F%253E%250A%253CHeading%2520headingType%253D%2522h2%2522%2520headingText%253D%2522Heading%2520level%25202%2522%252F%253E%250A%253CHeading%2520headingType%253D%2522h3%2522%2520headingText%253D%2522Heading%2520level%25203%2522%252F%253E%250A%253CHeading%2520headingType%253D%2522h4%2522%2520headingText%253D%2522Heading%2520level%25204%2522%252F%253E%250A%253CHeading%2520headingType%253D%2522h5%2522%2520headingText%253D%2522Heading%2520level%25205%2522%252F%253E%250A%253CHeading%2520headingType%253D%2522h6%2522%2520headingText%253D%2522Heading%2520level%25206%2522%252F%253E%250A"
+            style={{
+              width: "868px",
+              height: "420px",
               border: 0,
               transform: "scale(1)",
               overflow: "hidden",
-              }}
-              sandbox="allow-scripts allow-same-origin"
-            ></iframe>
-          </code>
-        </div>
+            }}
+            sandbox="allow-scripts allow-same-origin"
+          ></iframe>
+        </code>
       </div>
     </>
   );
--- src/documentation/TextDocs/TextDocs.jsx
+++ src/documentation/TextDocs/TextDocs.jsx
@@ -1,9 +1,42 @@
 import "./TextDocs.css";
+import { Text } from "../../components/Text/Text";
 
 export const TextDoc = () => {
   return (
     <>
-      <h2>Text docs</h2>
+      <div className="text-docs-container">
+        <div>
+          <h2>Text</h2>
+          <p>
+            In a document or webpage, the text component is used to show text or
+            paragraphs in different sizes for presentation and emphasis. To
+            increase readability, use the font-size CSS property. When the text
+            element is used well, it improves user engagement and visual
+            hierarchy while guaranteeing that the material is presented in an
+            aesthetically pleasing manner.
+          </p>
+        </div>
+        <div className="text-showcase">
+          <Text size="2xl" text="This is 2xl text." />
+          <Text size="xl" text="This is xl text." />
+          <Text size="lg" text="This is large text." />
+          <Text size="base" text="This is normal text." />
+          <Text size="sm" text="This is small text." />
+        </div>
+        <code>
+          <iframe
+            src="https://carbon.now.sh/embed?bg=rgba%28229%2C223%2C214%2C1%29&t=seti&wt=sharp&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%2520%253CText%2520size%253D%25222xl%2522%2520text%253D%2522This%2520is%25202xl%2520text.%2522%252F%253E%250A%2520%253CText%2520size%253D%2522xl%2522%2520text%253D%2522This%2520is%2520xl%2520text.%2522%252F%253E%250A%2520%253CText%2520size%253D%2522lg%2522%2520text%253D%2522This%2520is%2520large%2520text.%2522%252F%253E%250A%2520%253CText%2520size%253D%2522base%2522%2520text%253D%2522This%2520is%2520normal%2520text.%2522%252F%253E%250A%2520%253CText%2520size%253D%2522sm%2522%2520text%253D%2522This%2520is%2520small%2520text.%2522%252F%253E%250A"
+            style={{
+              width: "737px",
+              height: "300px",
+              border: "0",
+              transform: "scale(1)",
+              overflow: "hidden",
+            }}
+            sandbox="allow-scripts allow-same-origin"
+          ></iframe>
+        </code>
+      </div>
     </>
   );
 };
