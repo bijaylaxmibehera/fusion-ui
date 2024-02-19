dd6681c0c2a206003a3d612c9ecea45185c82ae1
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2024-02-19 09:07:28 +0530

    feat: heading docs completed

--- src/documentation/AlertDocs/AlertDocs.jsx
+++ src/documentation/AlertDocs/AlertDocs.jsx
@@ -29,7 +29,7 @@ export const AlertDocs = () => {
           <iframe
             src="https://carbon.now.sh/embed?bg=rgba%28229%2C223%2C214%2C1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253CAlert%2520type%253D%2522success%2522%2520text%253D%2522success%2520alert%2522%2520%252F%253E%250A%253CAlert%2520type%253D%2522info%2522%2520text%253D%2522info%2520alert%2522%2520%252F%253E%250A%253CAlert%2520type%253D%2522warning%2522%2520text%253D%2522warning%2520alert%2522%252F%253E%250A%253CAlert%2520type%253D%2522error%2522%2520text%253D%2522error%2522%2520%252F%253E%250A"
             style={{
-              width: "612px",
+              width: "590px",
               height: "289px",
               border: "0",
               transform: "scale(1)",
